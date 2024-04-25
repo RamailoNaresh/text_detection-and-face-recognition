2f2d8ee3b4126a96a62ea954a14df431c51b586d
RamailoNaresh <naresh@ramailo.tech>
2024-04-25 12:40:15 +0545

    added face location detection and comparig document with document_name

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from app import views
 
 urlpatterns = [
     path("", views.text_detection_google_cloud_vision, name='analyze image'),
-    path("compare_face/", views.face_compare, name= 'compare face')
+    path("verify_document/", views.document_verification, name= 'compare face')
 ]
--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"secretVisionApiKey.json"
 def text_detection_google_cloud_vision(request):
     client = vision.ImageAnnotatorClient()
 
-    image_path = "/home/mango/Downloads/rajesh5.jpeg"
+    image_path = "/home/mango/Downloads/rajesh.jpeg"
     with open(image_path, "rb") as image_file:
         image_binary = image_file.read()
     image = vision.Image(content=image_binary)
@@ -25,17 +25,38 @@ def text_detection_google_cloud_vision(request):
 
 
 @api_view(["POST"])
-def face_compare(request):
-    selfie_image = request.FILES.get("selfie_image")
-    personal_card_image = request.FILES.get("personal_card_image")
-    if selfie_image and personal_card_image:
-        known_image = face_recognition.load_image_file(selfie_image)
-        unknown_image = face_recognition.load_image_file(personal_card_image)
-        known_image_encoding = face_recognition.face_encodings(known_image)[0]
-        unknown_image_encoding = face_recognition.face_encodings(unknown_image)[0]
-        results = face_recognition.compare_faces([known_image_encoding], unknown_image_encoding)
-        probability = 1- face_recognition.face_distance([known_image_encoding], unknown_image_encoding)
-        if results[0] == True:
-            return JsonResponse({"is_matched": str(results[0]), "matched_probability": probability[0], "is_error": "False"})
-        return JsonResponse({"is_matched": str(results[0]), "matched_probability": probability[0], "is_error": "True"})
-    return JsonResponse({"is_error": "True"})
+def document_verification(request):
+    try:
+        selfie_image = request.FILES.get("selfie_image")
+        personal_card_image = request.FILES.get("personal_card_image")
+        document_name = request.POST["document_name"]
+        if selfie_image and personal_card_image and document_name:
+            client = vision.ImageAnnotatorClient()
+            # comparing document type from photo and text
+            image = vision.Image(content = personal_card_image.read())
+            label_detection_feature = vision.Feature(type_=vision.Feature.Type.TEXT_DETECTION)
+            request = vision.AnnotateImageRequest(image=image, features=[label_detection_feature])
+            response = client.annotate_image(request=request)
+            text_annotate = response.text_annotations
+            text = text_annotate[0].description
+            if document_name not in text:
+                return JsonResponse({"message": "Document type doesn't matched", "is_error": "False"})
+            # comparing faces in an image
+            known_image = face_recognition.load_image_file(selfie_image)
+            unknown_image = face_recognition.load_image_file(personal_card_image)
+            # checking face location
+            known_image_face_location = face_recognition.face_locations(known_image)
+            unknown_image_face_location = face_recognition.face_locations(unknown_image)
+            if not known_image_face_location or not unknown_image_face_location:
+                return JsonResponse({"message": "Please upload the document with face", "is_error": "True"})
+            known_image_encoding = face_recognition.face_encodings(known_image)[0]
+            unknown_image_encoding = face_recognition.face_encodings(unknown_image)[0]
+            results = face_recognition.compare_faces([known_image_encoding], unknown_image_encoding)
+            probability = 1- face_recognition.face_distance([known_image_encoding], unknown_image_encoding)
+
+            if results[0] == True:
+                return JsonResponse({"message": "Face matched","is_matched": str(results[0]), "matched_probability": probability[0], "is_error": "False"})
+            return JsonResponse({"message":"Face didn't matched","is_matched": str(results[0]), "matched_probability": probability[0], "is_error": "True"})
+        return JsonResponse({"message": "Fields cannot be empty","is_error": "True"})
+    except Exception as e:
+        return JsonResponse({"message":str(e),"is_error": "True"})
